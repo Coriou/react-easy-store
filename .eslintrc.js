6681e9627679109eae0d06204e86483443e2cb24
Benjamin C <benjamin@coriou.net>
2019-10-24 09:36:02 +0200

    Eslint: streamline rules values

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,8 @@ module.exports = {
 	},
 	plugins: ["react-hooks"],
 	rules: {
-		"react-hooks/rules-of-hooks": ["error"],
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
 		"no-unused-vars": ["warn"]
 	},
 	settings: {
